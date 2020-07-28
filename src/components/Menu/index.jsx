4bbde3daac6b4edc33041109c05fe58fa3ccab43
LucasCancio <lucas.cancio7@gmail.com>
2020-07-28 19:06:43 -0300

    Roteamento e State - Aula 2

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "../../assets/animeflix-logo.png";
 import Button from "../Button";
 //import ButtonLink from "./components/ButtonLink";
@@ -8,11 +9,11 @@ import "./Menu.css";
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="animeflix logo"></img>
-      </a>
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo video
       </Button>
     </nav>
